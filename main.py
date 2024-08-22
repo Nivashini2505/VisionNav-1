b283e5be13c952c1270faedbf0edc9ad76181c44
Abbilaash <abbilaashat@gmail.com>
2024-08-22 22:21:46 +0530

    print detection and confidence, issue reported

--- main.py
+++ main.py
@@ -6,24 +6,30 @@ import cv2
 # Custom function to handle predictions and print detected objects
 def custom_on_prediction(predictions, frame):
     try:
-        print(f"Predictions: {predictions}")
-        print(type(predictions))
+        # print(f"Predictions: {predictions}")
 
-        '''for prediction in predictions:
-            print(f"Content of prediction: {prediction}") #image
+        for prediction in predictions['predictions']:
+            print(f"Prediction: {prediction['class']} and confidence: {prediction['confidence']}")
 
-            print(f"Detected object: {prediction['class']} with confidence {prediction['confidence']:.2f}")'''
         
         # Render the bounding boxes on the frame
         render_boxes(predictions, frame)
         
         # Display the frame with predictions
         # TASK: SOME ERROR IS HERE => FIX IT
+        '''
+        Error in custom_on_prediction: OpenCV(4.8.0) :-1: error: (-5:Bad argument) in function 'imshow'
+        > Overload resolution failed:
+        >  - mat is not a numpy array, neither a scalar
+        >  - Expected Ptr<cv::cuda::GpuMat> for argument 'mat'
+        >  - Expected Ptr<cv::UMat> for argument 'mat'
+        '''
+        
         cv2.imshow('Predictions', frame)
         
         # Add a delay to allow the frame to be displayed
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            return
+            exit()
         
     except Exception as e:
         print(f"Error in custom_on_prediction: {e}")
@@ -33,7 +39,7 @@ try:
     pipeline = InferencePipeline.init(
         model_id="living-room-items/1",  # Replace with your Roboflow model ID
         video_reference=1,  # 0 to use the built-in webcam
-        api_key="",  # Your Roboflow API key
+        api_key="G0HdUVNRRR2g5eGLP20Z",  # Your Roboflow API key
         on_prediction=custom_on_prediction,  # Use the custom function for predictions
     )
 
